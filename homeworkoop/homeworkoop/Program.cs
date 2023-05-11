5e68026da42cf717aa7759b520087d7f241c97ce
unknown <129381997+DIYOR77@users.noreply.github.com>
2023-05-11 12:17:24 +0500

    Mijozga xizmat tartit bilan.

--- homeworkoop/homeworkoop/Program.cs
+++ homeworkoop/homeworkoop/Program.cs
@@ -4,8 +4,23 @@
     {
         static void Main(string[] args)
         {
-           
-            
+            Queue<string> queue = new Queue<string>();
+
+            queue.Enqueue(" Fayzullayev Abdulla ");
+            queue.Enqueue(" Akbarov Sarvar ");
+            queue.Enqueue(" Nurullayev Asadbek ");
+            queue.Enqueue(" Sharipov Aziz ");
+            queue.Enqueue(" Akbarov Azam ");
+            queue.Enqueue(" Nosirov Xurshid ");
+            queue.Enqueue(" Olimov Sattor ");
+
+            for (int i = 0; i < 7; i++)
+            {
+                Console.WriteLine(" Mijoz : " + (i + 1) + queue.Dequeue());
+                Console.ReadLine();
+                Console.Clear();
+            }
+
         }
     }
 }
